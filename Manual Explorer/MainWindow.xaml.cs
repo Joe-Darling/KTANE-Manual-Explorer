cfe9e1089aec5823870805cfe80f08d1a6dac2ad
sofi1247 <sofi1247@gmail.com>
2020-07-06 22:32:19 +0200

    Search done except complete spellchecking

--- Manual Explorer/MainWindow.xaml.cs
+++ Manual Explorer/MainWindow.xaml.cs
@@ -36,12 +36,20 @@ namespace Manual_Explorer
         private void UpdateQuery(object sender, KeyEventArgs e)
         {
             ComboBox comboBox = (ComboBox)sender;
-            //if (e.Key == Key.Back)
+            comboBox.Items.Clear();
 
-            if (e.Key >= Key.A && e.Key <= Key.Z)
+            if ((e.Key >= Key.A && e.Key <= Key.Z) || (e.Key >= Key.D0 && e.Key <= Key.D9))
             {
-                string userInput = comboBox.Text + e.Key.ToString().ToLower();
-                comboBox.Items.Clear();
+                string userInput = comboBox.Text.ToLower() + e.Key.ToString().ToLower();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (userInput.Contains(i.ToString()))
+                    {
+                        userInput = userInput.Remove(userInput.Length - 2, 1);
+                        break;
+                    }
+                }
 
                 List<string> contains = new List<string>();
                 List<string> wordStarts = new List<string>();
@@ -50,10 +58,10 @@ namespace Manual_Explorer
                 {
                     if (nameToCheck.StartsWith(userInput))
                     {
-                        comboBox.Items.Add(nameToCheck);
+                        comboBox.Items.Add(CapitilizeComboBoxItems(nameToCheck));
                     }
 
-                    if (nameToCheck.Contains(" "))
+                    else if (nameToCheck.Contains(" "))
                     {
                         string[] words = nameToCheck.Split(' ');
                         for (int i = 1; i < words.Length; i++)
@@ -61,40 +69,98 @@ namespace Manual_Explorer
                             if (words[i].StartsWith(userInput))
                             {
                                 wordStarts.Add(nameToCheck);
-                                //comboBox.Items.Add(nameToCheck);
                             }
                         }
-
                     }
 
                     else if (nameToCheck.Contains(userInput))
                     {
                         contains.Add(nameToCheck);
-                        //combobox.items.add(nametocheck);
                     }
                 }
 
+
                 foreach (string moduleName in wordStarts)
                 {
-                    comboBox.Items.Add(moduleName);
+                    comboBox.Items.Add(CapitilizeComboBoxItems(moduleName));
                 }
 
                 foreach (string moduleName in contains)
                 {
-                    comboBox.Items.Add(moduleName);
+                    comboBox.Items.Add(CapitilizeComboBoxItems(moduleName));
+                }
+
+                foreach (string item in modules.Keys)
+                {
+                    if (Compute(item, userInput) <= 3)
+                    {
+                        comboBox.Items.Add(CapitilizeComboBoxItems(item));
+                    }
+
                 }
 
             }
-            else if(e.Key == Key.LeftShift || e.Key == Key.Space)
+            else if (e.Key == Key.LeftShift || e.Key == Key.Space || e.Key == Key.RightShift)
             {
 
             }
             else
             {
-                comboBox.Text = comboBox.Text.Substring(0, comboBox.Text.Length - 1);
+                e.Handled = true;
                 Trace.WriteLine("A letter was NOT pressed");
             }
+
             comboBox.IsDropDownOpen = true;
+        }
+
+        public string CapitilizeComboBoxItems(string item)
+        {
+            return char.ToUpper(item[0]) + item.Substring(1);
+        }
+
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
         }
 
         private void OnSelected(object sender, SelectionChangedEventArgs e)
