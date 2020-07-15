425bd9be8831e3f576cc79b967126d86c0924dd6
sofi1247 <sofi1247@gmail.com>
2020-07-15 19:03:26 +0200

    New class created

---
+++ Manual Explorer/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manual_Explorer
+{
+    class Class1
+    {
+    }
+}
---
+++ Manual Explorer/SearchFunctionality.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manual_Explorer
+{
+    class SearchFunctionality
+    {
+
+    }
+}
